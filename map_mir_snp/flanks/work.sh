c7ef359c2c452e61808cf6336adcf8b04aa08438
fuxins <fuxinhust@gmail.com>
2019-03-17 07:53:41 +0800

    flanks coverage

---
+++ map_mir_snp/flanks/work.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/fux/fux/miRNASNP3/data/dbsnp/VCF
+bedfiles=$(ls x*.bed)
+for bed in ${bedfiles};do
+	bedtools coverage -a /home/fux/fux/miRNASNP3/data/miRbase/remap_ncbi/grch38p12.flanks.bed -b ${bed} > ${bed}.flank.coverage 
+done
+
+coveragefile=$(ls *.flank.coverage)
+for coverage in ${coveragefile};do
+	awk '$5>0{print $0}' ${coverage} >${coverage}.filter
+done
