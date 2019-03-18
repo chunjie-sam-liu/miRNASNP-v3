b8f0e079eb16e8e661081b1b6a172a10afa8e0df
fuxins <fuxinhust@gmail.com>
2019-03-18 14:40:07 +0800

    TargetScan

---
+++ target/work.sh
@@ -0,0 +1,2 @@
+#TargetScan
+nohup ../TargetScan/targetscan_70.pl /home/fux/fux/miRNASNP3/TargetScan/datasets/miR_family_info.txt /home/fux/fux/miRNASNP3/TargetScan/datasets/UTR_sequences_hsa.txt Targetscan_wildmir_result.txt 2>err &
