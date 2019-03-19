05644a9fd798fa5eebb33eb8f4dffc47f986142f
fuxins <fuxinhust@gmail.com>
2019-03-19 19:14:21 +0800

    seed region

--- map_mir_snp/seedregion/work_seedregion.sh
+++ map_mir_snp/seedregion/work_seedregion.sh
@@ -12,9 +12,12 @@
 #done
 
 #mv *.seedregion.coverage.filter /home/fux/fux/miRNASNP3/map_mir_snp/seedregion/
+
+#bedtools getfasta -fi /home/fux/fux/miRNASNP3/data/genome/hg38.fa -bed snp_in_seedregion.mir.bed -fo snp_in_seedregion.mir.fa
  
 awk '{print $4}' snp_in_seedregion |while read line
-do 
+do
+	#grep -w to ensure accurate matching 
 	grep ${line} /home/fux/fux/miRNASNP3/data/dbsnp/VCF/GCF_000001405.38.vcf >>snp_in_seedregion.vcf
 done
 
