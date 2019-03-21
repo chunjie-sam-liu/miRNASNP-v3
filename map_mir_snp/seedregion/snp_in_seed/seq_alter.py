4f6d390d9bc4c749db966c1dfaadc5f05bcd5c26
fuxins <fuxinhust@gmail.com>
2019-03-21 14:31:43 +0800

    seq_alter

---
+++ map_mir_snp/seedregion/snp_in_seed/seq_alter.py
@@ -0,0 +1,56 @@
+import re,os
+
+
+def getMir(rs):
+#	print("start grep")
+	cmd1 = "grep -w "+ rs + " snp_mir_relate"
+	output = os.popen(cmd1)
+	mirsnp = output.read().strip().split('\t')
+	dist = int(mirsnp[1]) -int(mirsnp[10])  #loci of snp -- loci of mir start_base
+	mir = {'id':mirsnp[14],'distance':dist,'strand':mirsnp[12]}
+	RULE1={"A":"T","C":"G","G":"C","U":"A"}
+	RULE2={"A":"A","C":"C","G":"G","U":"T"} 
+	with open("wild_mir.fa") as wildseq:
+		check_mir = wildseq.readline().strip()
+		while check_mir:
+			if check_mir.startswith('>'):
+				mirid = re.split(r'\s',check_mir)[0][1:]
+				if mirid == mir['id']: #warning hsa-miR-573 in hsa-miR-5739 --cause error 
+					curseq = wildseq.readline().strip()
+					if mir['strand'] == '-':
+						new_seq = "".join(map(lambda x:RULE1[x],curseq))[::-1] #fit snp
+					else:
+						new_seq = "".join(map(lambda x:RULE2[x],curseq))
+					mir['seq'] = new_seq
+					return mir
+			check_mir = wildseq.readline().strip()
+
+
+item = 0
+with open("alter_mir.fa","a") as alter:
+	with open("snv") as snv:
+		cursnp = snv.readline().strip()
+		while cursnp:
+			cursnpinfo = cursnp.split('\t')
+#			print("get rs:"+cursnpinfo[2])
+			mir = getMir(cursnpinfo[2])
+#			print(mir)
+			ref = cursnpinfo[3]
+			alt = cursnpinfo[4].split(',')
+			item+=len(alt)
+			for curalt in alt:
+				seq = list(mir['seq'])
+#				print("seq:" + str(seq)+"\n"+"ref:"+ref+">"+curalt+" curseq:" + seq[mir['distance']])
+				if seq[mir['distance']] == ref:
+					seq[mir['distance']] = curalt
+					altseq = ''.join(seq)
+					alter.write(">"+mir['id']+"\t"+cursnpinfo[2]+"\n")
+					alter.write(altseq + "\n")
+			cursnp = snv.readline().strip()
+					
+			
+print("Count items:"+str(item)) 
+			
+
+	
+	 	
