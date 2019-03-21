eda5d467698a8b9190bdf42ca9c7096c3e24a926
fuxins <fuxinhust@gmail.com>
2019-03-21 14:34:30 +0800

    pipe

--- map_mir_snp/seedregion/work_seedregion.sh
+++ map_mir_snp/seedregion/work_seedregion.sh
@@ -15,9 +15,18 @@
 
 #bedtools getfasta -fi /home/fux/fux/miRNASNP3/data/genome/hg38.fa -bed snp_in_seedregion.mir.bed -fo snp_in_seedregion.mir.fa
  
-awk '{print $4}' snp_in_seedregion |while read line
-do
+#awk '{print $4}' snp_in_seedregion |while read line
+#do
 	#grep -w to ensure accurate matching 
-	grep ${line} /home/fux/fux/miRNASNP3/data/dbsnp/VCF/GCF_000001405.38.vcf >>snp_in_seedregion.vcf
-done
+	#grep -w ${line} /home/fux/fux/miRNASNP3/data/dbsnp/VCF/GCF_000001405.38.vcf >>snp_in_seedregion.vcf
+#	grep -w ${line} snp_in_seedregion.vcf >>snp_in_seedregion_b.vcf
+#done
 
+#awk '{OFS='\t'}{if($1!~/^NW|^NT/)print $0}' snp_in_seedregion_b.vcf >snp_in_seedregion_c.vcf
+
+#cat rs_seed.add|while read line
+#do 
+#	grep -w ${line} /home/fux/fux/miRNASNP3/data/dbsnp/VCF/GCF_000001405.38.vcf >>snp_in_seedregion_d.vcf
+#done
+
+awk '{OFS='\t'}{if($1!~/^NW|^NT/)print $0}' snp_in_seedregion_d.vcf >snp_in_seedregion_d_1.vcf
