fdb20d46d64c8de2c8229a4f578e0a5e9d569ee1
fuxins <fuxinhust@gmail.com>
2019-03-21 19:00:46 +0800

    write converse

--- map_mir_snp/seedregion/snp_in_seed/seq_alter.py
+++ map_mir_snp/seedregion/snp_in_seed/seq_alter.py
@@ -8,8 +8,8 @@ def getMir(rs):
 	mirsnp = output.read().strip().split('\t')
 	dist = int(mirsnp[1]) -int(mirsnp[10])  #loci of snp -- loci of mir start_base
 	mir = {'id':mirsnp[14],'distance':dist,'strand':mirsnp[12]}
-	RULE1={"A":"T","C":"G","G":"C","U":"A"}
-	RULE2={"A":"A","C":"C","G":"G","U":"T"} 
+	RULE1={"A":"U","C":"G","G":"C","U":"A"}
+#	RULE2={"A":"A","C":"C","G":"G","U":"T"} 
 	with open("wild_mir.fa") as wildseq:
 		check_mir = wildseq.readline().strip()
 		while check_mir:
@@ -20,7 +20,7 @@ def getMir(rs):
 					if mir['strand'] == '-':
 						new_seq = "".join(map(lambda x:RULE1[x],curseq))[::-1] #fit snp
 					else:
-						new_seq = "".join(map(lambda x:RULE2[x],curseq))
+						new_seq = curseq
 					mir['seq'] = new_seq
 					return mir
 			check_mir = wildseq.readline().strip()
@@ -28,6 +28,8 @@ def getMir(rs):
 
 item = 0
 with open("alter_mir.fa","a") as alter:
+	RULE1={"A":"U","C":"G","G":"C","U":"A"}
+	RULE2={"A":"A","C":"C","G":"G","T":"U"}
 	with open("snv") as snv:
 		cursnp = snv.readline().strip()
 		while cursnp:
@@ -35,16 +37,19 @@ with open("alter_mir.fa","a") as alter:
 #			print("get rs:"+cursnpinfo[2])
 			mir = getMir(cursnpinfo[2])
 #			print(mir)
-			ref = cursnpinfo[3]
+			ref = RULE2[cursnpinfo[3]]
 			alt = cursnpinfo[4].split(',')
 			item+=len(alt)
 			for curalt in alt:
 				seq = list(mir['seq'])
 #				print("seq:" + str(seq)+"\n"+"ref:"+ref+">"+curalt+" curseq:" + seq[mir['distance']])
 				if seq[mir['distance']] == ref:
-					seq[mir['distance']] = curalt
-					altseq = ''.join(seq)
-					alter.write(">"+mir['id']+"\t"+cursnpinfo[2]+"\n")
+					seq[mir['distance']] = RULE2[curalt]
+					if mir['strand'] == '-':
+						altseq = "".join(map(lambda x:RULE1[x],seq))[::-1]
+					else:
+						altseq = ''.join(seq)
+					alter.write(">"+mir['id']+"_"+cursnpinfo[2]+"\n")
 					alter.write(altseq + "\n")
 			cursnp = snv.readline().strip()
 					
