5f3821bc5ccb61dc7896bd4cabdaa07c1ef9d4ce
fuxins <fuxinhust@gmail.com>
2019-03-25 12:16:39 +0800

    loadjson

--- data/dbsnp/VCF/json/loadjson.py
+++ data/dbsnp/VCF/json/loadjson.py
@@ -1,28 +1,32 @@
 import json,os,re
 
-#for root,dirs,files in os.walk("/home/fux/fux/miRNASNP3/data/dbsnp/VCF/json"):
-#	for fi in files:
-#		if fi.endswith('.pvcf.json'):
-#			with open(fi) as fjson:
-#				chr_dict = json.load(fjson)
-#				chrRegex = re.compile(r'NC_0*([1-9]*0?)')
-#				chrid = chrRegex.search(fi).group(1)
-#				with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr.chr"+chrid+".vcf","a") as out:
-#					with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr.chr"+chrid) as snp:
-#						line =  snp.readline().strip()
-#						while(line):
-#							snpid = line.split('\t')[3]
-#							out.write(chr_dict[snpid])
+for root,dirs,files in os.walk("/home/fux/fux/miRNASNP3/data/dbsnp/VCF/json"):
+	for fi in files:
+		if fi.endswith('.pvcf.json'):
+			with open(fi) as fjson:
+				chr_dict = json.load(fjson)
+				chrRegex = re.compile(r'NC_0*([1-9]*0?)')
+				chrid = chrRegex.search(fi).group(1)
+				with open("/home/fux/fux/miRNASNP3/map_utr3_snp/vcf_b/snp_in_utr3.chr"+chrid+".vcf","a") as out:
+					with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr3.chr"+chrid) as snp:
+						line =  snp.readline().strip()
+						while(line):
+							snpid = line.split('\t')[3]
+							out.write(chr_dict[snpid])
+							out.write("\n")
+							line =  snp.readline().strip()
 							
-with open("NC_000022.11.pvcf.json") as fjson:
-	chr_dict = json.load(fjson)
-	chrRegex = re.compile(r'NC_0*([1-9]*0?)')
-	chrid = chrRegex.search("NC_000022.11.pvcf.json").group(1)
-	with open("/home/fux/fux/miRNASNP3/map_utr3_snp/vcf_b/snp_in_utr3.chr"+chrid+".vcf","a") as out:
-		with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr3.chr"+chrid) as snp:
-			line =  snp.readline().strip()
-			while(line):
-				snpid = line.split('\t')[3]
-				out.write(chr_dict[snpid])
+#with open("NC_000022.11.pvcf.json") as fjson:
+#	chr_dict = json.load(fjson)
+#	chrRegex = re.compile(r'NC_0*([1-9]*0?)')
+#	chrid = chrRegex.search("NC_000022.11.pvcf.json").group(1)
+#	with open("/home/fux/fux/miRNASNP3/map_utr3_snp/vcf_b/snp_in_utr3.chr"+chrid+".vcf","a") as out:
+#		with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr3.chr"+chrid) as snp:
+#			line =  snp.readline().strip()
+#			while(line):
+#				snpid = line.split('\t')[3]
+#				out.write(chr_dict[snpid])
+#				out.write("\n")
+#				line =  snp.readline().strip()
 
 						
