4a31cc37102b726498d21126f467d175da720061
fuxins <fuxinhust@gmail.com>
2019-03-25 08:31:09 +0800

    loadjson

--- data/dbsnp/VCF/json/loadjson.py
+++ data/dbsnp/VCF/json/loadjson.py
@@ -1,5 +1,28 @@
-import json
+import json,os,re
 
-with open("chr24.json") as infile:
-	chr24_dict = json.load(infile)
-	print(len(chr24_dict))
+#for root,dirs,files in os.walk("/home/fux/fux/miRNASNP3/data/dbsnp/VCF/json"):
+#	for fi in files:
+#		if fi.endswith('.pvcf.json'):
+#			with open(fi) as fjson:
+#				chr_dict = json.load(fjson)
+#				chrRegex = re.compile(r'NC_0*([1-9]*0?)')
+#				chrid = chrRegex.search(fi).group(1)
+#				with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr.chr"+chrid+".vcf","a") as out:
+#					with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr.chr"+chrid) as snp:
+#						line =  snp.readline().strip()
+#						while(line):
+#							snpid = line.split('\t')[3]
+#							out.write(chr_dict[snpid])
+							
+with open("NC_000022.11.pvcf.json") as fjson:
+	chr_dict = json.load(fjson)
+	chrRegex = re.compile(r'NC_0*([1-9]*0?)')
+	chrid = chrRegex.search("NC_000022.11.pvcf.json").group(1)
+	with open("/home/fux/fux/miRNASNP3/map_utr3_snp/vcf_b/snp_in_utr3.chr"+chrid+".vcf","a") as out:
+		with open("/home/fux/fux/miRNASNP3/map_utr3_snp/snp_in_utr3.chr"+chrid) as snp:
+			line =  snp.readline().strip()
+			while(line):
+				snpid = line.split('\t')[3]
+				out.write(chr_dict[snpid])
+
+						
