bb55ec18c7d568f24b346d43f80ecbcd3a5c0225
fuxins <fuxinhust@gmail.com>
2019-03-25 13:20:45 +0800

    predict_02

---
+++ target/TargetScan/work.sh
@@ -0,0 +1,4 @@
+#TargetScan
+nohup ../TargetScan/targetscan_70.pl /home/fux/fux/miRNASNP3/TargetScan/datasets/miR_family_info.txt /home/fux/fux/miRNASNP3/TargetScan/datasets/UTR_sequences_hsa.txt Targetscan_wildmir_result.txt 2>err &
+
+nohup /home/fux/fux/miRNASNP3/TargetScan/targetscan_70.pl mir_seed_2_8.txt /home/fux/fux/miRNASNP3/TargetScan/datasets/UTR_sequences_hsa.txt Targetscan_wildmir_result_02.txt 2>err &
