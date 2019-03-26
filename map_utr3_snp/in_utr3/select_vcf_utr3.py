3645ec7b85aadb2ed5c7ddeee42fed2d39c4eed4
fuxins <fuxinhust@gmail.com>
2019-03-26 17:12:34 +0800

    utr snv

---
+++ map_utr3_snp/in_utr3/select_vcf_utr3.py
@@ -0,0 +1,22 @@
+import re
+with open("snp_in_utr3.sum","a") as out: 
+	with open("/home/fux/fux/miRNASNP3/map_utr3_snp/vcf_b/snp_in_utr3.final.vcf") as vcf:
+		line = vcf.readline().strip()
+		while line:
+			item = line.split()
+			gene = re.findall(r";GENEINFO=(.+?);",item[7])
+			vc = re.findall(r";VC=([A-Z]+)",item[7])
+			if not gene:
+				gene = "non"
+			else:
+				gene = gene[0]
+			if not vc:
+				vc = "non"
+			else:
+				vc = vc[0]
+			newline = item[0] + "\t" + item[1] + "\t"+ item[2] + "\t" + item[3] + "\t" + item[4] + "\t" + gene + "\t" + vc + "\n"
+			out.write(newline)
+#			print(gene)
+			line = vcf.readline().strip()
+
+
