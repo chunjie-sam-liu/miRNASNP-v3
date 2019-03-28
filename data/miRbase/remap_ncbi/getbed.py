47932f0033343017bf6177e45a1be4f6936b5a87
fuxins <fuxinhust@gmail.com>
2019-03-28 12:19:40 +0800

    pos5 eq 0

--- data/miRbase/remap_ncbi/getbed.py
+++ data/miRbase/remap_ncbi/getbed.py
@@ -1,15 +1,17 @@
 import re
 
+item = 0
 with open("mir.b.bed","a") as newbed:
 	with open("grch38p12_hsa.gff3") as gff3:
 		for line in gff3:
 			if line:
+				item+=1
 				line = line.strip()
 				if line.startswith('#') or line.startswith('NW_'):
 					continue
 				line = line.split('\t')
 				mirid = re.findall(r';Name=(.*);gbkey=',line[8])[0]
-				newline = line[0]+'\t'+line[3]+'\t'+line[4]+'\t'+line[2]+":"+mirid+'\t'+line[6]+'\n'
+				newline = line[0]+'\t'+line[3]+'\t'+line[4]+'\t'+line[2]+":"+mirid+'\t0\t'+line[6]+'\n'
 				newbed.write(newline)
 #				print(line[8]+'\n')
-#				print(mirid)				
+		print("items:"+str(item))				
