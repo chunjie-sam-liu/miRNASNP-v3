c5db6b23492187fb3a494097827a00289c5bf07e
fuxins <fuxinhust@gmail.com>
2019-04-05 15:10:25 +0800

    flanks relate

--- map_mir_snp/flanks/work.sh
+++ map_mir_snp/flanks/work.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cd /home/fux/fux/miRNASNP3/data/dbsnp/VCF
-bedfiles=$(ls x*.bed)
-for bed in ${bedfiles};do
-	bedtools coverage -a /home/fux/fux/miRNASNP3/data/miRbase/remap_ncbi/grch38p12.flanks.bed -b ${bed} > ${bed}.flank.coverage 
-done
+#cd /home/fux/fux/miRNASNP3/data/dbsnp/VCF
+#bedfiles=$(ls x*.bed)
+#for bed in ${bedfiles};do
+#	bedtools coverage -a /home/fux/fux/miRNASNP3/data/miRbase/remap_ncbi/grch38p12.flanks.bed -b ${bed} > ${bed}.flank.coverage 
+#done
 
-coveragefile=$(ls *.flank.coverage)
-for coverage in ${coveragefile};do
-	awk '$5>0{print $0}' ${coverage} >${coverage}.filter
-done
+#coveragefile=$(ls *.flank.coverage)
+#for coverage in ${coveragefile};do
+#	awk '$5>0{print $0}' ${coverage} >${coverage}.filter
+#done
+
+bedtools intersect -a grch38p21.flank.cov.bed -b /home/fux/fux/miRNASNP3/data/miRbase/remap_ncbi/mirflanks.b.bed -wa -wb >flanks_mir_relate
