6acc0ecfbb744dc2dce3bef10b3294ac1a2c0772
fuxins <fuxinhust@gmail.com>
2019-04-05 08:43:14 +0800

    flanks

--- data/miRbase/remap_ncbi/getbed.py
+++ data/miRbase/remap_ncbi/getbed.py
@@ -1,8 +1,8 @@
 import re
 
 item = 0
-with open("mir.b.bed","a") as newbed:
-	with open("grch38p12_hsa.gff3") as gff3:
+with open("mirflanks.b.bed","a") as newbed:
+	with open("grch38p12_hsa.gff3.primary") as gff3:
 		for line in gff3:
 			if line:
 				item+=1
@@ -11,7 +11,8 @@ with open("mir.b.bed","a") as newbed:
 					continue
 				line = line.split('\t')
 				mirid = re.findall(r';Name=(.*);gbkey=',line[8])[0]
-				newline = line[0]+'\t'+line[3]+'\t'+line[4]+'\t'+line[2]+":"+mirid+'\t0\t'+line[6]+'\n'
-				newbed.write(newline)
+				newline1 = line[0]+'\t'+str(int(line[3])-1000)+'\t'+line[3]+'\t'+line[2]+":upstream:"+mirid+'\t0\t'+line[6]+'\n'
+				newline2 = line[0]+'\t'+line[4]+'\t'+str(int(line[4])+1000)+'\t'+line[2]+":downstream:"+mirid+'\t0\t'+line[6]+'\n'
+				newbed.write(newline1+newline2)
 #				print(line[8]+'\n')
 		print("items:"+str(item))				
