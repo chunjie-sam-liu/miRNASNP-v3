9d1799d2de750abc0b6e727bd915af2fe0fa27f2
fuxins <fuxinhust@gmail.com>
2019-04-08 19:01:20 +0800

    s3

--- target/miranda/s3_gethit.py
+++ target/miranda/s3_gethit.py
@@ -1,9 +1,9 @@
 import re
 
-with open("hit_a.txt","a") as hit:
+with open("altmir_hit.txt","a") as hit:
 	hit.write("mirid\tutr_chr\tutr_start-end\tutr_strand\thit_counts\n")
 	chit=0
-	with open("s1_miranda_res_02_a.txt") as res:
+	with open("s1_miranda_altmir_res_02.txt") as res:
 		for line in res:
 			if line:	
 				if line.startswith('Performing Scan:'):
@@ -16,6 +16,7 @@ with open("hit_a.txt","a") as hit:
 					chit+=1
 				elif line.startswith('Complete'):
 					hit.write(str(chit-1)+'\n')
+					chit=0
 					
 				
 #			print(line)	
