2ed30bd5b04a7008720c7a827d32c28ec7b9ae22
fuxins <fuxinhust@gmail.com>
2019-05-10 19:49:43 +0800

    有框还没写具体的预测函数

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/tools.controller.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/tools.controller.js
@@ -1,8 +1,14 @@
 "use strict";
-
 angular.module('miRNASNP3')
     .controller('ToolsController', ToolsController);
 
 function ToolsController($scope) {
     console.log("ToolsController loaded");
+    $scope.predict = function () {
+        alert("Predict!");
+    }
+    $scope.load = function () {
+        alert("Example!");
+    }
 }
+
