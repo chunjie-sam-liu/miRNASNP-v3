4a9c5d03b1e659523378e6c7ee1e55c6e452a1f4
fuxins <fuxinhust@gmail.com>
2019-05-12 14:36:50 +0800

    home

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/home.controller.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/home.controller.js
@@ -5,31 +5,9 @@ angular.module('miRNASNP3')
 
 function HomeController($scope,$http,$rootScope) {
     console.log("HomeController loaded");
-    $scope.search_query = function (){
-        $http({
-        url: '/api/gain_hit',
-        method: 'GET',
-        params: {search_ids: $scope.search_ids}
-
-    }).then(
-        function (response) {
-            console.log(response);
-            $rootScope.hit_list = response.data.hit_list;
-        });
+    $scope.search_query = function () {
+        $rootScope.search_ids = $scope.search_ids
     }
-     $scope.fetch_hit_list = function () {
-        $http({
-            url: '/api/gain_hit',
-            method: 'GET',
-            params: {page: 1, per_page: 30}
-        }).then(
-            function (response) {
-                console.log(response);
-                $scope.hit_list = response.data.hit_list;
-            }
-        )};
-
-    $scope.fetch_hit_list();
 }
 
 
