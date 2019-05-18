3a270012395941aabca2d5bc00fb2e198588f76e
fuxins <fuxinhust@gmail.com>
2019-05-18 11:41:45 +0800

    home

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/home.controller.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/home.controller.js
@@ -3,10 +3,26 @@
 angular.module('miRNASNP3')
     .controller('HomeController', HomeController);
 
-function HomeController($scope,$http,$rootScope) {
+function HomeController($scope,$http,$rootScope,$window) {
     console.log("HomeController loaded");
+    var snp_pattern=/^rs[0-9]*/;
+    var mir_pattern=/^hsa/;
+    var mutation_pattern=/[a-z]/;
+    var gene_pattern=/[a-z]/;
+
     $scope.search_query = function () {
         $rootScope.search_ids = $scope.search_ids
+        $scope.$watch('search_ids',function (newVal) {
+            if(newVal==""){
+                window.alert("Enpty input")
+            }
+            else if(snp_pattern.test(newVal)){
+                window.open("#!/search_summary","_parent")
+            }
+            else if (mir_pattern.test(newVal)){
+                window.open("#!/search_summary_mir","_parent")
+            }
+        })
     }
 }
 
