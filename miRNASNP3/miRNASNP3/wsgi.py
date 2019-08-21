f8c7a3edd659ec42f2e1056cae76e3654980d399
fuxins <fuxinhust@gmial.com>
2019-08-21 22:25:42 +0800

    add path

--- miRNASNP3/miRNASNP3/wsgi.py
+++ miRNASNP3/miRNASNP3/wsgi.py
@@ -2,5 +2,7 @@ import sys
 #sys.path.insert(0, '/home/liut/github/EVmiRNA')
 
 #from EVmiRNA import app as application
+sys.path.append('/home/fux/web/miRNASNP3/miRNASNP3/miRNASNP3')
+sys.path.append('/home/fux/pro-fu/db-mir-snp/miRmap-1.1/src')
 
 from miRNASNP3 import app as application
