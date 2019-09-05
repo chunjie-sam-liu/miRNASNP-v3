a6ea79102e37ff23dc5dca7f5ff5b5901c4c3baf
fuxins <fuxinhust@gmial.com>
2019-09-05 11:23:11 +0800

    load new image:title,mirna,snp,mutation

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/mutation_summary.controller.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/mutation_summary.controller.js
@@ -13,22 +13,17 @@ function MutationSummaryController($scope,$routeParams,$http,$filter,miRNASNP3Se
     var condition={};
     var base_url = miRNASNP3Service.getAPIBaseUrl();
     var gene=$routeParams.gene
-    $scope.target_effection=$routeParams.target_effection
     $scope.initial=1;
 
     $scope.fetch_mutation_summary=function(page){
 
         $scope.mutation_summary_count=0;
-        $scope.target_effection=0;
-        $scope.total_summary=1;
         condition['chrome']='All';
         condition['location']='All';
         condition['resource']='All';
         condition["snp_rela"]='';
         condition['pubmed_id']='';
         condition['page']=page
-
-        $scope.target_effection=$routeParams.target_effection
 
         if (gene){
             condition['gene']=gene
@@ -58,15 +53,12 @@ function MutationSummaryController($scope,$routeParams,$http,$filter,miRNASNP3Se
         if ($("#snp_rela").is(":checked")){
             condition["snp_rela"]=1
         }
-        if ($("#is_pubmed").is(":checked")){
-            condition['pubmed_id']=1
-        }
-        if ($("#target_effection").is(":checked")||$scope.target_effection==1){
-            $scope.target_effection=1;
-            $scope.total_summary=0;
-        }
+        //if ($("#is_pubmed").is(":checked")){
+        //    condition['pubmed_id']=1
+        //}
+        
 
-        condition['target_effection']=$scope.target_effection;
+        //condition['target_effection']=$scope.target_effection;
         console.log(condition)
         $http({
             //url:base_url+'/api/mutation_summary',
@@ -81,8 +73,8 @@ function MutationSummaryController($scope,$routeParams,$http,$filter,miRNASNP3Se
             var data_list=$scope.mutation_summary_list
             for(var i=0;i<data_list.length;i++){
                 data_list[i].pathology_show=data_list[i].pathology.replace(/,/g,"; ").replace(/_and/g," ").replace(/_/g," ").replace(/\|/g,"; ")
-                if(data_list[i].resource=="clinvar"){data_list[i].resource="ClinVar"}
-                if(data_list[i].resource=="cosmic"){data_list[i].resource="COSMIC"}
+                if(data_list[i].resource=="clinvar"){data_list[i].resource="ClinVar";data_list[i].url="https://www.ncbi.nlm.nih.gov/clinvar/variation/"+data_list[i].mut_id}
+                if(data_list[i].resource=="cosmic"){data_list[i].resource="COSMIC";data_list[i].url="https://cancer.sanger.ac.uk/cosmic/ncv/overview?id="+data_list[i].mut_id.replace(/COSN/g,"")}
                 if(data_list[i].location=="UTR3"){data_list[i].location="3'UTR"}
             }
         })
