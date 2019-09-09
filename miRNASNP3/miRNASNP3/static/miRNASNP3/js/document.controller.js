82d383bcd59bfd3d9a6031ff0b02611253dac13a
fuxins <fuxinhust@gmial.com>
2019-09-09 20:43:57 +0800

    document page

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/document.controller.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/document.controller.js
@@ -5,4 +5,43 @@ angular.module('miRNASNP3')
 
 function DocumentController($scope) {
     console.log("DocumentController loaded");
+    function removeClasshtml(){
+        $("#intro").css({"backgroundColor":"","color":"darkred"});
+        $("#qs").css({"backgroundColor":"","color":"darkred"});
+        $("#ds").css({"backgroundColor":"","color":"darkred"});
+    }
+    
+    $scope.gotoAnchor = function(x){
+      var newHash = 'anchor' + x;
+			var id = $location.hash();
+      if ($location.hash() !== newHash) {
+        // set the $location.hash to `newHash` and
+        // $anchorScroll will automatically scroll to it
+        $location.hash('anchor' + x);
+				$anchorScroll();
+				$location.hash(id);
+      } else {
+        // call $anchorScroll() explicitly,
+        // since $location.hash hasn't changed
+        $anchorScroll();
+      }
+      switch(x){
+          case 1: 
+          removeClasshtml();
+          $("#intro").css({"backgroundColor":"#0088cc","color":"GhostWhite"});
+          break;
+          case 2:
+          removeClasshtml();
+          $("#qs").css({"backgroundColor":"#0088cc","color":"GhostWhite"});
+          break;
+          case 3:
+          removeClasshtml();
+          $("#ds").css({"backgroundColor":"#0088cc","color":"GhostWhite"});
+          break;
+          case 4:
+          removeClasshtml();
+          $("#dl").css({"backgroundColor":"#0088cc","color":"GhostWhite"});
+          break;
+      }
+    };
 }
