39beae1a3d7f1964df344fbdf66b91eef31fc076
fuxins <fuxinhust@gmial.com>
2019-09-09 20:50:51 +0800

    delet contact

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/contact.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('miRNASNP3')
-    .controller('ContactController', ContactController);
-
-function ContactController($scope) {
-    console.log("ContactController loaded");
-}
