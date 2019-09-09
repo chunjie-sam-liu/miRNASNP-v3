4e4699f832d6bad6b62ad9f5e9f3c1633ece9ec9
fuxins <fuxinhust@gmial.com>
2019-09-09 20:47:25 +0800

    division primir and mature

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/home.controller.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/home.controller.js
@@ -12,97 +12,41 @@ function HomeController($scope,$http,$routeParams,$window) {
             $("#search_button").trigger("click");
         }
     });
-    $(".input-xs:text").each(function () {
-        jQuery(this).change(function () {
-          jQuery(this).val(jQuery.trim(jQuery(this).val()));
-        })
-      })
-
-    //var snp_pattern = /^rs[0-9]*/i;
-    //var mir_pattern = /(^hsa|^mir|^let)/i;
-    //var cosmic_pattern = /^COS[0-9]*/i;
-    //var gene_pattern = //;
-    var flag = 0;
+    
 
     $scope.search_query = function () {
-        var query_item = $('#search').val();
+        var flag = 0;
+        $scope.alert_nonitem=0;
+        $scope.alert_invalid=0;
+        var query_item = $.trim($('#search').val());
         console.log(query_item);
-        if (/[@#\$%\^&\*<>\.]+/g.test(query_item)) {
-            //alert("Invalid input");
+        if (/[@#\$%\^&\*<>\.]+/g.test(query_item)) {  
             flag = 1;
-            //$scope.alert_illegal=1;
-            //$('#alert_illegal').show()
-            alert("invalid input !")
-            //history.back();
-        }
+            $scope.alert_invalid=1;
+            $('#alert_invalid').show()
+            //window.open( "#!/", "_self");
+            console.log($scope.alert_invalid)
+                }
+        
         if (flag == 0) {
             console.log("flag==0");
             if (query_item.indexOf('rs')==0) {
                 console.log("a rs");
                 $scope.filter_snp(query_item)
             }
-            //if (cosmic_pattern.test(query_item)) {
-             //   $scope.filter_cosmic(query_item)
-            //}
             if (query_item.indexOf('hsa')==0||query_item.indexOf("miR")==0||query_item.indexOf("let")==0||query_item.indexOf("mir")==0) {
                 console.log("a mir")
                 $scope.filter_mirna(query_item)
             }
-            //if (query_item.indexOf("ENST") == 0) {
-            //    $scope.filter_transcript(query_item)
-           // }
             if(!(query_item.indexOf("rs")==0)&&!(query_item.indexOf("hsa")==0 || query_item.indexOf("miR")==0|| query_item.indexOf("let")==0||query_item.indexOf("mir")==0)){
                 console.log('unknown')
                 $scope.filter_alias(query_item)
             }
         }
-    };
-        $scope.filter_alias = function (query_item) {
-            /*$scope.alert_unrecorgnize=1;
-            //$('#alert_unrecorgize').show()
-            $http({
-                url:'/api/snp_summary',
-                method:'GET',
-                params:{gene:query_item}
-            }).then(function(response){
-                console.log(response)
-                $scope.snp_summary_list=response.data.snp_summary_list;
-                $scope.snp_summary_count=response.data.snp_summary_count;
-            })
-            $http({
-                url:'/api/mutation_summary',
-                method:'GET',
-                params:{gene:query_item,target_effection:1}
-            }).then(function(response){
-                console.log(response)
-                $scope.mutation_summary_list=response.data.mutation_summary_list;
-                $scope.mutation_summary_count=response.data.mutation_summary_count[0].count;
-            })
-            $http({
-                url:'/api/snp_seed_gain',
-                method:'GET',
-                params:{gene:query_item}
-            }).then(function(response){
-                console.log(response)
-                $scope.snp_seed_gain_list=response.data.snp_seed_gain_list;
-                $scope.snp_seed_gain_count=response.data.snp_seed_gain_count;
-            })
-            $http({
-                url:'/api/snp_seed_loss',
-                method:'GET',
-                params:{gene:query_item}
-            }).then(function(response){
-                console.log(response)
-                $scope.snp_seed_loss_list=response.data.snp_seed_loss_list
-                $scope.snp_seed_loss_count=response.data.snp_seed_loss_count
-            })
-            if(Number($scope.snp_summary_count)==0 && Number($scope.mutation_summary_count)==0 && Number($scope.snp_seed_gain_count)==0 && Number($scope.snp_seed_loss_count)==0){
-                console.log('noitem!')
-                $scope.alert_nonitem=1;
-                $('#alert_nonitem').show()
-            }else{*/
+    }
+
+        $scope.filter_alias = function (query_item) {  
                 window.open("#!/gene?query_gene="+query_item,"_self");
-            //}
         };
         $scope.filter_snp = function (query_snp) {
             $http({
@@ -122,9 +66,6 @@ function HomeController($scope,$http,$routeParams,$window) {
                     window.open( "#!/snp?snp_id=" + query_snp, "_self");
                 }
         })}
-       // $scope.filter_transcript = function (query_enst) {
-        //    window.open("#!/enst?enst=" + query_enst, "_self")
-        //};
         $scope.filter_mirna = function (query_mirna) {
             $http({
                 //url:base_url+'/api/mirna_key',
@@ -142,63 +83,6 @@ function HomeController($scope,$http,$routeParams,$window) {
                     window.open("#!/key?mirna_id=" + query_mirna, "_self")
                 }
         })}
-       // $scope.filter_cosmic = function (query_cosmic) {
-        //    window.open("#!/cosmic?cosmic_id=" + query_cosmic, "_self")
-        //};
-}
 
-/*angular.module('miRNASNP3')
-    .controller('NavController', NavController);
 
-function NavController($scope,$http,$routeParams,$window){
-    console.log("NavController loaded");
-    $(".alert").alert();
-    var snp_pattern = /^rs[0-9]i;
-    var mir_pattern = /(^hsa|^mir|^let)/i;
-    var cosmic_pattern = /^COS[0-9]i;
-    //var gene_pattern = //;
-    var flag = 0;
-
-    $scope.search_nav_query = function () {
-        var query_nav_item = $('#nav_search').val();
-        console.log(query_nav_item);
-        if (/[@#\$%\^&\*<>\.]+/g.test(query_nav_item)) {
-            alert("Invalid input");
-            flag = 1;
-            history.back();
-        }
-        if (flag == 0) {
-            console.log("flag==0");
-            if (snp_pattern.test(query_nav_item)) {
-                console.log("a rs");
-                $scope.filter_snp(query_nav_item)
-            } else if (cosmic_pattern.test(query_nav_item)) {
-                $scope.filter_cosmic(query_nav_item)
-            } else if (mir_pattern.test(query_nav_item)) {
-                $scope.filter_mirna(query_nav_item)
-            } //else if () {
-                //$scope.filter_transcript(query_nav_item)
-            //}
-        else {
-                $scope.filter_alias(query_nav_item)
-            }
-        }
-    };
-        $scope.filter_alias = function (query_item) {
-
-        };
-        $scope.filter_snp = function (query_snp) {
-            window.open( "#!/snp?snp_id=" + query_snp, "_self");
-        };
-        $scope.filter_transcript = function (query_enst) {
-            window.open("#!/enst?enst=" + query_enst, "_self")
-        };
-        $scope.filter_mirna = function (query_mirna) {
-            window.open("#!/key?mirna_id="+query_mirna,"_self")
-            //window.open("#!/mirna?mirna_id=" + query_mirna, "_self")
-        };
-        $scope.filter_cosmic = function (query_cosmic) {
-            window.open("#!/cosmic?cosmic_id=" + query_cosmic, "_self")
-        };
-}*/
-
+    }
