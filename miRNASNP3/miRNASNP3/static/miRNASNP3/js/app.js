14cccdec7af2b524a6da8977bcb5334a9eda6d07
fuxins <fuxinhust@gmial.com>
2019-09-21 10:47:10 +0800

    remove ngTagsInput

--- miRNASNP3/miRNASNP3/static/miRNASNP3/js/app.js
+++ miRNASNP3/miRNASNP3/static/miRNASNP3/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('miRNASNP3', ['ui.bootstrap', 'ngRoute', 'pageslide-directive', 'ui.bootstrap-slider', 'bw.paging','tableSort','ngTagsInput'])
+angular.module('miRNASNP3', ['ui.bootstrap', 'ngRoute', 'pageslide-directive', 'ui.bootstrap-slider', 'bw.paging','tableSort'])
     .config(function ($routeProvider) {
         $routeProvider
             .when("/", {
