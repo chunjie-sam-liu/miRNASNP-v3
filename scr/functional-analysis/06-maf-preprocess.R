d85844e02e075a6de4d4813eac5e7e5c3a63c8f6
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-11-07 07:01:43 +0000

    parse uuid and tcga barcode

--- scr/functional-analysis/06-maf-preprocess.R
+++ scr/functional-analysis/06-maf-preprocess.R
@@ -21,3 +21,29 @@ maf_filename <- list.files(path = path_snv, pattern = 'maf$', recursive = TRUE)
   useAll = TRUE
   )
 
+# Path --------------------------------------------------------------------
+
+path_manifest <- '/workspace/liucj/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/mirna-expression/gdc_manifest_mirna_expression.txt'
+manifest_tb <- readr::read_tsv(file = path_manifest)
+barcodes <- list.dirs(path = '/home/liucj/data/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/mirna-expression', full.names = FALSE, recursive = F)
+
+setdiff(manifest_tb$id, barcodes)
+manifest_tb %>% 
+  dplyr::filter(!id %in% barcodes) %>% 
+  readr::write_tsv(path = '/workspace/liucj/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/mirna-expression/gdc_manifest_mirna_expression.sup.txt')
+
+
+
+# uuid to barcode ---------------------------------------------------------
+
+
+files_endpoint = "https://api.gdc.cancer.gov/files"
+
+body = list("filters" = list("op" = "in", "content" = list('field'='files.file_id', 'value'=manifest_tb$id)), 'format' = 'TSV', 'fields'="file_id,file_name,cases.case_id,cases.submitter_id,cases.samples.sample_id,cases.samples.submitter_id,cases.samples.portions.analytes.aliquots.aliquot_id,cases.samples.portions.analytes.aliquots.submitter_id,cases.samples.sample_type,cases.samples.tissue_type,data_category,data_type", 'size' = length(manifest_tb$id))
+
+response <- httr::POST(url = files_endpoint, body = body, encode = 'json')
+
+res_table <- httr::content(x = response, as = 'parsed', type = 'text/tab-separated-values')
+readr::write_tsv(x = res_table, path = '/home/liucj/data/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/mirna-expression/manifest-uuid-barcode.tsv')
+
+
