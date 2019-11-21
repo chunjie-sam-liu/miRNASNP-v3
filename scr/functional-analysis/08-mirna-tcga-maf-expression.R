46fb63769b60638e9d7034c5a59916a6547d956e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-11-21 10:53:39 +0800

    mirna-tcga maf

--- scr/functional-analysis/08-mirna-tcga-maf-expression.R
+++ scr/functional-analysis/08-mirna-tcga-maf-expression.R
@@ -2,3 +2,25 @@
 # Library -----------------------------------------------------------------
 
 library(magrittr)
+
+# Path --------------------------------------------------------------------
+
+path_mirna_mutation <- '/home/liucj/data/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/snv/mirna-mutation'
+maffiles <- list.files(path = path_mirna_mutation, pattern = '*maf.premir.format')
+
+
+# Function ----------------------------------------------------------------
+
+fn_load <- function(.x) {
+  .d <- readr::read_tsv(file = file.path(path_mirna_mutation, .x), col_names = F)
+  .d
+}
+
+maffiles %>% 
+  purrr::map(.f = fn_load) ->
+  mirna_maf_mutation
+
+
+mirna_maf_mutation %>% 
+  purrr::map(ncol)
+
