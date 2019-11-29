6a0906d06eb2c9db6ac0db0a221fcc013648d03e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2019-11-29 17:26:36 +0800

    stat tcga

--- scr/functional-analysis/06-maf-preprocess.R
+++ scr/functional-analysis/06-maf-preprocess.R
@@ -47,3 +47,40 @@ res_table <- httr::content(x = response, as = 'parsed', type = 'text/tab-separat
 readr::write_tsv(x = res_table, path = '/home/liucj/data/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/mirna-expression/manifest-uuid-barcode.tsv')
 
 
+
+
+
+# MAF sample statistics ---------------------------------------------------
+
+
+snv_path <- '/workspace/liucj/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/snv'
+
+maf_files <- list.files(path = snv_path, pattern = 'maf$', recursive = T)
+
+fn_callback <- function(.y) {
+  .y <- gsub(pattern = '#tumor.aliquots.submitter_id ', replacement = '', x = .y)
+  .xx <- stringr::str_split(string = .y, pattern = ',', simplify = T)[1, ]
+  
+  tibble::tibble(
+    Tumor_Sample_Barcode = .xx
+  ) %>% 
+    dplyr::distinct() %>% 
+    dplyr::mutate(barcode = stringr::str_sub(string = Tumor_Sample_Barcode, start = 1, end= 16)) %>% 
+    dplyr::mutate(sample = stringr::str_sub(string = barcode, start = 1, end = 12))
+}
+
+maf_files %>% 
+  purrr::map(.f = function(.x) {
+    .cancer <- gsub(
+      pattern = '\\.',
+      replacement = '-',
+      x = gsub(pattern = '.*/(TCGA.+)\\.mutect.*', replacement = '\\1', x = .x)
+    )
+    .x_file <- file.path(snv_path, .x)
+    readr::read_lines(file = .x_file, skip = 4, n_max = 1) %>% 
+      fn_callback() %>% 
+      dplyr::mutate(cancers = .cancer, n_sample = length(unique(barcode)))
+  }) %>% 
+  dplyr::bind_rows() -> 
+  maf_data
+readr::write_rds(x = maf_data, path = '/home/liucj/data/refdata/tcga-somatic-mutation-and-mirna-expression-grch38/snv/mirna-mutation/maf-sample-statistics.rds.gz', compress = 'gz')
