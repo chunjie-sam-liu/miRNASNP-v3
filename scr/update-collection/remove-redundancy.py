70ecca049f9ce82dd4434a9969c58306c9bcdc26
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-07-20 11:37:24 +0800

    update mongo

--- scr/update-collection/remove-redundancy.py
+++ scr/update-collection/remove-redundancy.py
@@ -11,111 +11,87 @@ import json
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
-# connect to
-THE_COLLECTION = "seed_gain_4666"
-NEW_COLLECTION = THE_COLLECTION + "_cj"
 
-# resource connection
-mongo = MongoClient("mongodb://username:password@localhost:port/dbname")
+class MongoManipulation:
+    __mongo = MongoClient("mongodb://username:passwd@ip:port/dbname")
 
+    def __init__(self, col_name):
+        self.__col_name = col_name
+        self.__new_col_name = col_name + "_cj"
 
-def get_data(condition={}, output={"_id": 0}, col_name=THE_COLLECTION):
-    """
-    Connect to MongoDB and get data from collection "seed_gain_4666"
-    """
-    mcur = mongo.mirnasnp[col_name].find(condition, output)
-    return list(mcur)
+    def get_data(self, condition={}, output={"_id": 0}):
+        mcur = self.__mongo.mirnasnp[self.__col_name].find(
+            condition, output, no_cursor_timeout=True
+        )
+        return list(mcur)
 
+    def insert_data(self, d={}):
+        if d:
+            self.__mongo.mirnasnp[self.__new_col_name].insert_one(d)
 
-def insert_into_mongo(d={}, col_name=NEW_COLLECTION):
-    """[summary]
-
-    Args:
-        d ([type]): [description]
-    """
-    if d:
-        mongo.mirnasnp[col_name].insert_one(d)
+    def drop_col(self):
+        if self.__new_col_name in self.__mongo.mirnasnp.list_collection_names():
+            self.__mongo.mirnasnp[self.__new_col_name].drop()
 
 
-def drop_new_col(col_name=NEW_COLLECTION):
-    if col_name in mongo.mirnasnp.list_collection_names():
-        mongo.mirnasnp[col_name].drop()
+class DataUpdate(MongoManipulation):
+    def __init__(self, col_name):
+        MongoManipulation.__init__(self, col_name)
 
+    def traverse_all_mirna_snp_gene(self):
+        ds = self.get_data(
+            output={"_id": 0, "mirna_id": 1, "snp_id": 1, "gene_symbol": 1}
+        )
+        ds_u = map(dict, set(tuple(sorted(d.items())) for d in ds))
+        for d in ds_u:
+            self.__get_single_item(cond=d)
 
-def key_words():
-    """
-    keywords: mirna_id, snp_id, gene_symbol
-    refseq: utr_info.acc
-    mirmap: site_info.mm_start, site_info.mm_end, site_info.tgs_start, site_info.tgs_end
-    """
-    pass
+    def __get_single_item(self, cond):
+        ds = self.get_data(condition=cond)
+        the_keys_u = set(self.__make_key_from_target_site(d) for d in ds)
+        the_keys_u_d = {v: [] for v in the_keys_u}
 
+        for d in ds:
+            the_key = self.__make_key_from_target_site(d)
+            acc = d["utr_info"]["acc"]
+            if not isinstance(acc, list):
+                the_keys_u_d[the_key] += [acc]
 
-def make_key_from_target_site(d):
-    return "#".join(
-        [
-            d["site_info"]["mm_start"],
-            d["site_info"]["mm_end"],
-            d["site_info"]["tgs_start"],
-            d["site_info"]["tgs_end"],
-        ]
-    )
-
-
-def get_single_item(cond):
-    ds = get_data(condition=cond)
-
-    the_keys_u = set(make_key_from_target_site(d) for d in ds)
-    the_keys_u_d = {v: [] for v in the_keys_u}
-
-    for d in ds:
-        the_key = make_key_from_target_site(d)
-        the_keys_u_d[the_key] += [d["utr_info"]["acc"]]
-
-    for k, v in the_keys_u_d.items():
-        cond.update(
-            dict(
-                zip(
-                    [
-                        "site_info.mm_start",
-                        "site_info.mm_end",
-                        "site_info.tgs_start",
-                        "site_info.tgs_end",
-                    ],
-                    k.split("#"),
+        for k, v in the_keys_u_d.items():
+            cond.update(
+                dict(
+                    zip(
+                        [
+                            "site_info.mm_start",
+                            "site_info.mm_end",
+                            "site_info.tgs_start",
+                            "site_info.tgs_end",
+                        ],
+                        k.split("#"),
+                    )
                 )
             )
+            ds_u = self.get_data(cond)
+            res = ds_u[0]
+            res["utr_info"]["acc"] = v
+            # insert to mongodb
+            self.insert_data(d=res)
+
+    def __make_key_from_target_site(self, d):
+        return "#".join(
+            [
+                d["site_info"]["mm_start"],
+                d["site_info"]["mm_end"],
+                d["site_info"]["tgs_start"],
+                d["site_info"]["tgs_end"],
+            ]
         )
-
-        ds_u = get_data(cond)
-        res = ds_u[0]
-        res["utr_info"]["acc"] = v
-
-        # insert to mongodb
-        insert_into_mongo(d=res)
-
-
-def traverse_all_mirna_snp_gene():
-    """
-    test mirna snp gene
-    hsa-miR-105-3p rs778443619 SERBP1
-    """
-    # return datasets
-    ds = get_data(output={"_id": 0, "mirna_id": 1, "snp_id": 1, "gene_symbol": 1})
-
-    # unique datasets
-    ds_u = map(dict, set(tuple(sorted(d.items())) for d in ds))
-
-    for d in ds_u:
-        """
-        d is dict of gene_symbol, mirna_id, snp_id
-        """
-        get_single_item(cond=d)
 
 
 def main():
-    drop_new_col()
-    traverse_all_mirna_snp_gene()
+    col1 = DataUpdate(col_name="seed_gain_4666")
+    col1.drop_col()
+    col1.traverse_all_mirna_snp_gene()
 
 
 if __name__ == "__main__":
